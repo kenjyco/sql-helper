b0923d60a7af20563a6f7f81f3c7be46cb4b3854
Ken <kenjyco@gmail.com>
2019-05-14 22:43:19 -0500

    Bump to v0.0.5

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open('README.rst', 'r') as fp:
 
 setup(
     name='sql-helper',
-    version='0.0.4',
+    version='0.0.5',
     description='Helper funcs and tools for working with SQL in mysql or postgresql',
     long_description=long_description,
     author='Ken',
     author_email='kenjyco@gmail.com',
     license='MIT',
     url='https://github.com/kenjyco/sql-helper',
-    download_url='https://github.com/kenjyco/sql-helper/tarball/v0.0.4',
+    download_url='https://github.com/kenjyco/sql-helper/tarball/v0.0.5',
     packages=find_packages(),
     install_requires=[
         'SQLAlchemy==1.3.2',
