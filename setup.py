7e34e6608625b8638489ef34f78a78f172d251b0
Ken <kenjyco@gmail.com>
2022-04-10 15:47:50 -0500

    Update keywords and classifiers in setup.py

--- setup.py
+++ setup.py
@@ -34,11 +34,16 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Software Development :: Libraries',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python',
+        'Programming Language :: SQL',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
     ],
-    keywords=['sql', 'mysql', 'postgresql', 'sqlite', 'helper']
+    keywords=['sql', 'data', 'database', 'cli', 'command-line', 'sqlalchemy', 'mysql', 'postgresql', 'sqlite', 'helper', 'kenjyco']
 )
