15ed9b9e91dd608babbba403beb26fde88226de8
Ken <kenjyco@gmail.com>
2023-06-22 00:28:08 -0500

    Update help text and add --confirm-exit option to sql-ipython script

--- sql_helper/scripts/sql_ipython.py
+++ sql_helper/scripts/sql_ipython.py
@@ -5,22 +5,27 @@ import sql_helper as sqh
 
 @click.command()
 @click.option(
-    '--no-colors', '-c', 'no_colors', is_flag=True, default=False,
-    help='Do not use shell colors in ipython'
-)
-@click.option(
-    '--no-vi', '-v', 'no_vi', is_flag=True, default=False,
+    '--no-vi', 'no_vi', is_flag=True, default=False,
     help='Do not use vi editing mode in ipython'
 )
-def main(no_colors, no_vi):
+@click.option(
+    '--no-colors', 'no_colors', is_flag=True, default=False,
+    help='Do not use colors / syntax highlighting in ipython'
+)
+@click.option(
+    '--confirm-exit', 'confirm_exit', is_flag=True, default=False,
+    help='Prompt "Do you really want to exit ([y]/n)?" when exiting ipython'
+)
+def main(**kwargs):
     """Start an ipython session with an instance of an SQL object"""
     selected = sqh.select_url_from_settings()
     if selected:
         sql = sqh.SQL(selected, attempt_docker=True, wait=True)
         ih.start_ipython(
             warn=True,
-            colors=not no_colors,
-            vi=not no_vi,
+            colors=not kwargs['no_colors'],
+            vi=not kwargs['no_vi'],
+            confirm_exit=kwargs['confirm_exit'],
             sql=sql
         )
     else:
